eefc9241a418322c08b24c67cfe1a472214a7798
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-02-25 18:26:08 +0600

    add attributes in config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    app_name: str = "EDU BOT"
-    admin_email: str
+    # attribute mentioned here must be present in .env as well
+    app_name: str
+    db_host:str
+    db_name:str
+    db_secret:str
+    db_port:int|str
+    
     model_config = SettingsConfigDict(env_file=".env") # reads .env file
 
 settings = Settings()
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from typing import Union
 from fastapi import FastAPI
-from dbconfig import SQLModel, engine
+# from dbconfig import SQLModel, engine
 from config import settings
 
-from app.api.v1.routes import user
+# from app.api.v1.routes import user
 
-SQLModel.metadata.create_all(engine) #connect to DB and create tables
+# SQLModel.metadata.create_all(engine) #connect to DB and create tables
 
 app = FastAPI()
 
 # Register routers
-app.include_router(user.router)
+# app.include_router(user.router)
 
 @app.get("/")
 async def home():
-    return {"message": f"Welcome to the {settings.app_name} API!"}
+    return {"message": f"Welcome to the {settings.APP_NAME} API!"}
